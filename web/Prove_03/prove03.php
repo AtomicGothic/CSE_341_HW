d992adeb33fbc1a01fb6b5db345fbb2a8f2cf018
AtomicGothic <58697172+AtomicGothic@users.noreply.github.com>
2020-05-05 14:34:14 -0400

    Added my own template for assignment.

---
+++ web/Prove_03/prove03.php
@@ -0,0 +1,62 @@
+<?php
+
+    $firstName = $_POST['first_name'];
+    $lastName = $_POST['last_name'];
+    $address = $_POST['address'];
+    $phoneNumber = $_POST['phone'];
+    $merchandise = $_POST['item'];
+    $total = $_POST['total'];
+    $cardType = $_POST['card'];
+    $cardExp = date_create_from_format("m#Y", $_POST['exp_date']);
+
+    
+
+    echo "<!DOCTYPE html>
+    <html lang=\"en-us\">
+    <head>
+        <meta charset=\"utf-8\">
+        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+        <link rel=\"stylesheet\" href=\"prove03_styleSheet.css\">
+        <title>Confirm Order</title>
+        <script type=\"text/javascript\" src=\"prove03_javaScript.js\"></script>
+    </head>
+    <body>
+        <div class=\"standardText\">
+            <h2>Please Review Your Order</h2><br></br>
+            <p class=\"main\">Name: " . $firstName . " " . $lastName . "</p><br>
+            <p class=\"main\">Shipping Address: " . $address . "</p><br>
+            <p class=\"main\">Phone: " . $phoneNumber . "</p><br>";
+
+      echo  "<p class=\"main\">Your Cart: <br> ";
+       
+    if(!empty($merchandise))
+    {
+        foreach ($merchandise as $cart)
+        {
+            echo "$" . $cart . "<br>";
+        }
+    }
+      
+      echo  "<p class=\"main\">Total: $" . $total . "</p><br>
+            <p class=\"main\">Card: " . $cardType . "</p><br>
+            <p class=\"main\">Expiry: ";
+
+      echo date_format($cardExp,"F Y");
+      
+      echo "</p><br></br>
+        </div>
+        <div class=\"standardText\">
+            <form class=\"main\" action=\"assign11_a.php\" method=\"POST\" onsubmit=\"return submitConfirmation()\">
+                <input id=\"submit_form\" type=\"submit\" value=\"Submit\" name=\"confirm\">
+                <input id=\"cancel_form\" type=\"submit\" value=\"Cancel\" name=\"cancel\"><br><br /><br />
+            </form>       
+         </div>
+        <footer class=\"standardText\">
+        <p>Posted by: Adam Goff</p>
+        <p>Contact information: <a href=\"mailto:quiryquark@byui.edu\" target=\"_blank\">quirkyquark@byui.edu</a>.</p><br>
+    </footer>
+    </body>
+    </html>";
+
+
+?>
---
+++ web/Prove_03/prove03_script.js
@@ -0,0 +1,90 @@
+function calculateTotal(id, value) {
+    let tempPrice;
+    if (document.getElementById(id).checked) {
+        tempPrice = parseFloat(document.getElementById('total').value) + parseFloat(value);
+        document.getElementById('total').value = tempPrice.toFixed(2);
+    }
+    else {
+        tempPrice = parseFloat(document.getElementById('total').value) - parseFloat(value);
+        document.getElementById('total').value = tempPrice.toFixed(2);
+    }
+}
+
+function validatePhoneNumber() {
+    let checkInput = new RegExp("^[0-9]{3}-[0-9]{3}-[0-9]{4}$")
+    let textInput = document.getElementsByClassName('inputField');
+
+    if (!checkInput.test(textInput[3].value)) {
+        textInput[3].style.color = 'red';
+        return false;
+    }
+    else {
+        textInput[3].style.color = 'black';
+        return true;
+    }
+}
+
+function validateCardNumber() {
+    let checkInput = new RegExp("^[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}$")
+    if (document.getElementById('amex_card').checked == true) {
+        checkInput = /^[0-9]{4}-[0-9]{6}-[0-9]{5}$/
+    }
+    else {
+        checkInput = /^[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}$/
+    }
+
+    let textInput = document.getElementsByClassName('inputField');
+
+    if (!checkInput.test(textInput[10].value)) {
+        textInput[10].style.color = 'red';
+        return false;
+    }
+    else {
+        textInput[10].style.color = 'black';
+        return true;
+    }
+}
+
+function validateCardExpiry() {
+    let checkInput = new RegExp("^(0[0-9]|1[0-2])/([2-9][0-9][1-9][8-9]|[2-9][0-9][2-9][0-9])$")
+    let textInput = document.getElementsByClassName('inputField');
+
+    if (!checkInput.test(textInput[11].value)) {
+        textInput[11].style.color = 'red';
+        return false;
+    }
+    else {
+        textInput[11].style.color = 'black';
+        return true;
+    }
+}
+
+function changeToUpperCase(id) {
+    let tempElement = document.getElementById(id);
+    tempElement.value = tempElement.value.toUpperCase();
+}
+
+function focusFirstElemet() {
+    document.getElementById('first_name').focus();
+}
+
+function submitForm() {
+    let formList = document.getElementsByClassName('inputField');
+
+    for (let i = 0; i < formList.length; i++) {
+        if (formList[i].value == "" || !validatePhoneNumber() || !validateCardNumber() || !validateCardExpiry()) {
+            formList[i].focus();
+            return false;
+        }
+    }
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "assign11.php", true);
+    xhttp.send();
+}
+
+function submitConfirmation() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "assign11_a.php", true);
+    xhttp.send();
+}
