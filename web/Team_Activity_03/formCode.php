9959487912a45c2956aae6af104b8dd5c3696a27
AtomicGothic <58697172+AtomicGothic@users.noreply.github.com>
2020-05-05 23:48:34 -0400

    The final try before surrender.

--- web/Team_Activity_03/formCode.php
+++ web/Team_Activity_03/formCode.php
@@ -4,14 +4,15 @@ $email = htmlspecialchars($_POST["userEmail"]);
 $major = htmlspecialchars($_POST["major"]);
 $comments = htmlspecialchars($_POST["comments"]);
 $places = $_POST["places"];
-​$information = array(
+​
+$information = array(
     "North America is pretty cool.  Lots of people live there" => "0",
     "South America is below North America.  It is very beautiful and has a very long and interesting history" => "1",
     "Europe is amazing.  It unfortunately has been the site of 2 World Wars.  There are many historical sites worth visiting though." => "2",
     "Asia is a very beautiful place.  There are many types of activities for all, between the nightlife, beaches, and quiet places to meditate." => "3",
     "Australia: Kangaroos, koalas, and the Great Barrier Reef. So many different places to visit." => "4",
     "Since you have visited Africa, you know that it is the 2nd largest continent in both land and in population.  There are only a few parts of the continent that you'd consider cold." => "5",
-    "Antarctica.  Guess what? Its cold.  And penguins.  Lots of ice.  But it has penguins!" => "6"
+    "Antarctica.  Guess what? Its cold.  And penguins.  Lots of ice.  But it has penguins!" => "6",
 );
 ?>
 ​
@@ -38,17 +39,15 @@ $places = $_POST["places"];
             }
             echo "</span></p>";
     ?>
- <h4>Basic info on places you've visited</h4>
- 
+    <h4>Basic info on places you've visited</h4>
 <?php
-      $countryNumber = 1;
+        $countryNumber = 1;
         foreach($places as $visited) {
             echo "<p>Country {$countryNumber}: ";
             echo array_search($visited, $information);
             echo "</p>";
             $countryNumber++;
-        }
-?>
-
+        };
+    ?>
 </body>
 </html>
