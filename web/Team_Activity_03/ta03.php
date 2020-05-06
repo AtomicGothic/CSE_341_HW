66767b948dfc2fc4004e866e6c56c0a709e038fc
AtomicGothic <58697172+AtomicGothic@users.noreply.github.com>
2020-05-05 23:21:01 -0400

    Added code for stretch challenge 2

--- web/Team_Activity_03/ta03.php
+++ web/Team_Activity_03/ta03.php
@@ -20,13 +20,13 @@
         <input type="radio" id="CE" name="major" value="Computer Engineering">
         <label for="CE">Computer Engineering</label><br><br>
         <!--Which countries did the user visit?-->
-        <input type="checkbox" name="places[]" id="northAmerica" value="North America"><label for="northAmerica">North America</label><br />
-        <input type="checkbox" name="places[]" id="southAmerica" value="South America"><label for="southAmerica">South America</label><br />
-        <input type="checkbox" name="places[]" id="europe" value="Europe"><label for="europe">Europe</label><br />
-        <input type="checkbox" name="places[]" id="asia" value="Asia"><label for="Asia">Asia</label><br />
-        <input type="checkbox" name="places[]" id="australia" value="Australia"><label for="australia">Australia</label><br />
-        <input type="checkbox" name="places[]" id="africa" value="Africa"><label for="africa">Africa</label><br />
-        <input type="checkbox" name="places[]" id="antarctica" value="Antarctica"><label for="Antarctica">Antarctica</label><br />
+        <input type="checkbox" name="places[]" id="northAmerica" value="0"><label for="northAmerica">North America</label><br />
+        <input type="checkbox" name="places[]" id="southAmerica" value="1"><label for="southAmerica">South America</label><br />
+        <input type="checkbox" name="places[]" id="europe" value="2"><label for="europe">Europe</label><br />
+        <input type="checkbox" name="places[]" id="asia" value="3"><label for="Asia">Asia</label><br />
+        <input type="checkbox" name="places[]" id="australia" value="4"><label for="australia">Australia</label><br />
+        <input type="checkbox" name="places[]" id="africa" value="5"><label for="africa">Africa</label><br />
+        <input type="checkbox" name="places[]" id="antarctica" value="6"><label for="Antarctica">Antarctica</label><br />
         <!--User thoughts-->
         <label for="comments"><textarea name="comments" cols="20" rows="4">Enter Your Comments...</textarea></label><br>
         <input class="button" type="submit" name="submit" value="Submit">
