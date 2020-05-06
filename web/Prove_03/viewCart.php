aed549353b18659b7755d7c9969527e836c5cfaf
AtomicGothic <58697172+AtomicGothic@users.noreply.github.com>
2020-05-06 15:09:13 -0400

    Fixed action php on viewCart.php

--- web/Prove_03/viewCart.php
+++ web/Prove_03/viewCart.php
@@ -40,7 +40,7 @@
         <br></br>
         </div>
         <div class="standardText">
-            <form class="main" action="cancellationPage.php" method="POST" onsubmit="return toCheckout()">
+            <form class="main" action="checkoutPage.php" method="POST" onsubmit="return toCheckout()">
                 <input id="submit_form" type="submit" value="Confirm" name="confirm">
                 <input id="cancel_form" type="submit" value="Cancel" name="cancel"><br><br /><br />
             </form>
