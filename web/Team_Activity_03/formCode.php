04825dd0a94ba0cbbf05ac8d20769027b9ed86af
AtomicGothic <58697172+AtomicGothic@users.noreply.github.com>
2020-05-05 22:20:12 -0400

    Added part 2 of Team Assignment 03

---
+++ web/Team_Activity_03/formCode.php
@@ -0,0 +1,24 @@
+<?php
+$name = htmlspecialchars($_POST["userName"]);
+$email = htmlspecialchars($_POST["userEmail"]);
+$major = htmlspecialchars($_POST["major"]);
+$comments = htmlspecialchars($_POST["comments"]);
+
+echo "
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+    <title>Team Activity 03</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+</head>
+<body id="mainBody">
+    <p><b>Name: </b><span><?php echo $name ?></span></p>
+    <p><b>Email: </b><span><a href="mailto:<?php echo $email ?>"><?php echo $email ?></a></span></p>
+    <p><b>Major: </b><span><?php echo $major ?></span></p>
+    <p><b>Comments: </b><span><?php echo $comments ?></span></p>
+</body>
+</html>
+"
+
+?>
