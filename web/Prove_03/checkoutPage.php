762bd85b78bb2c2c71586b57b9b660478e42b94d
AtomicGothic <58697172+AtomicGothic@users.noreply.github.com>
2020-05-06 23:10:14 -0400

    Reorganized layout of checkoutPage.php

--- web/Prove_03/checkoutPage.php
+++ web/Prove_03/checkoutPage.php
@@ -20,6 +20,11 @@
             <h3>What is your best contact number?</h3>
             <div>Phone: <input class="inputField" id="phone" style="background-color:#f7f7ff; text-align:right;" type="text" name="phone" value="xxx-xxx-xxxx" oninput="validatePhoneNumber()"><br><br /></div>
             <h3>Please Review Your Order</h3><br></br>
+            <input id="submit_form" type="submit" value="Confirm" name="confirm">
+        </form>
+        </div>
+        <div class="standardText">
+            <form class="main" action="viewCart.php" method="POST" onsubmit="return toCart()">
             <div>First Name: <input class="inputField" id="first_name" style="background-color:#f7f7ff; text-align:right;" type="text" name="first_name" value="<?php echo $_SESSION['firstName'];?>" readonly><br><br /></div>
             <div>Last Name: <input class="inputField" id="last_name" style="background-color:#f7f7ff; text-align:right;" type="text" name="last_name" value="<?php echo $_SESSION['lastName'];?>" readonly><br><br /></div>
             <?php
@@ -35,11 +40,6 @@
             ?>
             <div>Total: $<input id="total" class="inputField" style="background-color:#f7f7ff; text-align:right;" type="text" name="total" value="<?php echo $_SESSION['totalPrice']?>" readonly><br><br /></div>
             <br></br>
-            <input id="submit_form" type="submit" value="Confirm" name="confirm">
-        </form>
-        </div>
-        <div class="standardText">
-            <form class="main" action="viewCart.php" method="POST" onsubmit="return toCart()">
                 <input id="submit_form" type="submit" value="Back to Cart" name="backToCart">
             </form>
         </div>
