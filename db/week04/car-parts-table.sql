5f4a8e02ba272063f282e677fb7d922bd599a345
AtomicGothic <58697172+AtomicGothic@users.noreply.github.com>
2020-05-20 21:38:12 -0400

    Modified database.

--- db/week04/car-parts-table.sql
+++ db/week04/car-parts-table.sql
@@ -14,23 +14,14 @@ CREATE TABLE car_type (
 CREATE TABLE car_brakes (
     id SERIAL NOT NULL,
     car_type_id INT NOT NULL REFERENCES car_type(id),
-    disc_brake VARCHAR(256),
-    drum_brake VARCHAR(256),
-    parking_brake VARCHAR(256),
-    ABS_brake VARCHAR(256)
+    brake_pad VARCHAR(256)
 );
 
 CREATE TABLE car_tires (
     id SERIAL NOT NULL,
     car_type_id INT NOT NULL REFERENCES car_type(id),
     all_season VARCHAR(256),
-    all_terrain VARCHAR(256),
-    low_profile VARCHAR(256),
-    mud VARCHAR(256),
-    performance VARCHAR(256),
-    winter VARCHAR(256),
-    SUV VARCHAR(256),
-    truck VARCHAR(256)
+    winter VARCHAR(256)
 );
 
 -- Add Car Type Data
@@ -47,50 +38,6 @@ INSERT INTO car_type (car_make, car_model, car_year)
 INSERT INTO car_type (car_make, car_model, car_year)
     VALUES ('Toyota', 'Prius', '2020');
 
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Toyota', 'Avalon', '2016');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Toyota', 'Avalon', '2017');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Toyota', 'Avalon', '2018');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Toyota', 'Avalon', '2019');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Toyota', 'Avalon', '2020');
-
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Toyota', 'Corolla', '2016');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Toyota', 'Corolla', '2017');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Toyota', 'Corolla', '2018');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Toyota', 'Corolla', '2019');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Toyota', 'Corolla', '2020');
-
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Toyota', 'Tundra', '2016');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Toyota', 'Tundra', '2017');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Toyota', 'Tundra', '2018');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Toyota', 'Tundra', '2019');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Toyota', 'Tundra', '2020');
-
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Toyota', '4Runner', '2016');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Toyota', '4Runner', '2017');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Toyota', '4Runner', '2018');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Toyota', '4Runner', '2019');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Toyota', '4Runner', '2020');
-
 --Chevrolet
 INSERT INTO car_type (car_make, car_model, car_year)
     VALUES ('Chevrolet', 'Corvette', '2016');
@@ -99,65 +46,11 @@ INSERT INTO car_type (car_make, car_model, car_year)
 INSERT INTO car_type (car_make, car_model, car_year)
     VALUES ('Chevrolet', 'Corvette', '2018');
 INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Chevrolet', 'Corevette', '2019');
+    VALUES ('Chevrolet', 'Corvette', '2019');
 INSERT INTO car_type (car_make, car_model, car_year)
     VALUES ('Chevrolet', 'Corvette', '2020');
 
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Chevrolet', 'Malibu', '2016');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Chevrolet', 'Malibu', '2017');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Chevrolet', 'Malibu', '2018');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Chevrolet', 'Malibu', '2019');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Chevrolet', 'Malibu', '2020');
-
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Chevrolet', 'Equinox', '2016');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Chevrolet', 'Equinox', '2017');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Chevrolet', 'Equinox', '2018');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Chevrolet', 'Equinox', '2019');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Chevrolet', 'Equinox', '2020');
-
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Chevrolet', 'Colorado', '2016');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Chevrolet', 'Colorado', '2017');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Chevrolet', 'Colorado', '2018');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Chevrolet', 'Colorado', '2019');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Chevrolet', 'Colorado', '2020');
-
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Chevrolet', 'Express', '2016');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Chevrolet', 'Express', '2017');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Chevrolet', 'Express', '2018');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Chevrolet', 'Express', '2019');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Chevrolet', 'Express', '2020');
-
 --Ford
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Ford', 'Fusion', '2016');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Ford', 'Fusion', '2017');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Ford', 'Fusion', '2018');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Ford', 'Fusion', '2019');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Ford', 'Fusion', '2020');
 
 INSERT INTO car_type (car_make, car_model, car_year)
     VALUES ('Ford', 'Mustang', '2016');
@@ -170,50 +63,7 @@ INSERT INTO car_type (car_make, car_model, car_year)
 INSERT INTO car_type (car_make, car_model, car_year)
     VALUES ('Ford', 'Mustang', '2020');
 
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Ford', 'Explorer', '2016');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Ford', 'Explorer', '2017');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Ford', 'Explorer', '2018');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Ford', 'Explorer', '2019');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Ford', 'Explorer', '2020');
-
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Ford', 'F-150', '2016');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Ford', 'F-150', '2017');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Ford', 'F-150', '2018');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Ford', 'F-150', '2019');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Ford', 'F-150', '2020');
-
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Ford', 'Transit Connect', '2016');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Ford', 'Transit Connect', '2017');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Ford', 'Transit Connect', '2018');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Ford', 'Transit Connect', '2019');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Ford', 'Transit Connect', '2020');
-
 --Honda
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Honda', 'CR-V', '2016');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Honda', 'CR-V', '2017');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Honda', 'CR-V', '2018');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Honda', 'CR-V', '2019');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Honda', 'CR-V', '2020');
 
 INSERT INTO car_type (car_make, car_model, car_year)
     VALUES ('Honda', 'Civic', '2016');
@@ -226,59 +76,7 @@ INSERT INTO car_type (car_make, car_model, car_year)
 INSERT INTO car_type (car_make, car_model, car_year)
     VALUES ('Honda', 'Civic', '2020');
 
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Honda', 'Accord', '2016');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Honda', 'Accord', '2017');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Honda', 'Accord', '2018');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Honda', 'Accord', '2019');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Honda', 'Accord', '2020');
-
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Honda', 'Odyssey', '2016');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Honda', 'Odyssey', '2017');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Honda', 'Odyssey', '2018');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Honda', 'Odyssey', '2019');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Honda', 'Odyssey', '2020');
-
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Honda', 'Ridgeline', '2017');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Honda', 'Ridgeline', '2018');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Honda', 'Ridgeline', '2019');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Honda', 'Ridgeline', '2020');
-
 --Jeep
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Jeep', 'Renegade', '2016');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Jeep', 'Renegade', '2017');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Jeep', 'Renegade', '2018');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Jeep', 'Renegade', '2019');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Jeep', 'Renegade', '2020');
-
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Jeep', 'Cherokee', '2016');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Jeep', 'Cherokee', '2017');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Jeep', 'Cherokee', '2018');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Jeep', 'Cherokee', '2019');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Jeep', 'Cherokee', '2020');
 
 INSERT INTO car_type (car_make, car_model, car_year)
     VALUES ('Jeep', 'Wrangler', '2016');
@@ -291,24 +89,109 @@ INSERT INTO car_type (car_make, car_model, car_year)
 INSERT INTO car_type (car_make, car_model, car_year)
     VALUES ('Jeep', 'Wrangler', '2020');
 
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Jeep', 'Compass', '2016');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Jeep', 'Compass', '2017');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Jeep', 'Compass', '2018');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Jeep', 'Compass', '2019');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Jeep', 'Compass', '2020');
 
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Jeep', 'Grand Cherokee', '2016');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Jeep', 'Grand Cherokee', '2017');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Jeep', 'Grand Cherokee', '2018');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Jeep', 'Grand Cherokee', '2019');
-INSERT INTO car_type (car_make, car_model, car_year)
-    VALUES ('Jeep', 'Grand Cherokee', '2020');
+-- Add Car Brakes
+
+--Toyota Prius 2016
+INSERT INTO car_brakes (car_type_id, brake_pad)
+    VALUES (1, 'Front Brake: DYNAMIC FRICTION 1551118410 5000 Advanced');
+
+--Toyota Prius 2017
+INSERT INTO car_brakes (car_type_id, brake_pad)
+    VALUES (2, 'Front Brake: DYNAMIC FRICTION 1551118410 5000 Advanced');
+
+--Toyota Prius 2018
+INSERT INTO car_brakes (car_type_id, brake_pad)
+    VALUES (3, 'Front Brake: DYNAMIC FRICTION 1551118410 5000 Advanced');
+
+--Toyota Prius 2019
+INSERT INTO car_brakes (car_type_id, brake_pad)
+    VALUES (4, 'Front Brake: DYNAMIC FRICTION 1551118410 5000 Advanced');
+
+--Toyota Prius 2020
+INSERT INTO car_brakes (car_type_id, brake_pad)
+    VALUES (5, 'Front Brake: DYNAMIC FRICTION 1551118410 5000 Advanced');
+
+
+--Chevrolet Corvette 2016
+INSERT INTO car_brakes (car_type_id, brake_pad)
+    VALUES (6, 'Front Brake: DYNAMIC FRICTION 1553100100 5000 Advanced');
+
+--Chevrolet Corvette 2017
+INSERT INTO car_brakes (car_type_id, brake_pad)
+    VALUES (7, 'Front Brake: DYNAMIC FRICTION 1553100100 5000 Advanced');
+
+--Chevrolet Corvette 2018
+INSERT INTO car_brakes (car_type_id, brake_pad)
+    VALUES (8, 'Front Brake: DYNAMIC FRICTION 1553100100 5000 Advanced');
+
+--Chevrolet Corvette 2019
+INSERT INTO car_brakes (car_type_id, brake_pad)
+    VALUES (9, 'Front Brake: DYNAMIC FRICTION 1553100100 5000 Advanced');
+
+--Chevrolet Corvette 2020
+INSERT INTO car_brakes (car_type_id, brake_pad)
+    VALUES (10, 'Front Brake: ACDELCO 84197896 GM Original Equipment');
+
+
+--Ford Mustang 2016
+INSERT INTO car_brakes (car_type_id, brake_pad)
+    VALUES (11, 'Front Brake: CENTRIC 10517910 Posi-Quiet');
+
+--Ford Mustang 2017
+INSERT INTO car_brakes (car_type_id, brake_pad)
+    VALUES (12, 'Front Brake: CENTRIC 10517910 Posi-Quiet');
+
+--Ford Mustang 2018
+INSERT INTO car_brakes (car_type_id, brake_pad)
+    VALUES (13, 'Front Brake: DYNAMIC FRICTION 1551178400 5000 Advanced');
+
+--Ford Mustang 2019
+INSERT INTO car_brakes (car_type_id, brake_pad)
+    VALUES (14, 'Front Brake: DYNAMIC FRICTION 1551178400 5000 Advanced');
+
+--Ford Mustang 2020
+INSERT INTO car_brakes (car_type_id, brake_pad)
+    VALUES (15, 'Front Brake: DYNAMIC FRICTION 1551178400 5000 Advanced');
+
+
+--Honda Civic 2016
+INSERT INTO car_brakes (car_type_id, brake_pad)
+    VALUES (16, 'Front Brake: MONROE CX914 Total Solution Ceramic');
+
+--Honda Civic 2017
+INSERT INTO car_brakes (car_type_id, brake_pad)
+    VALUES (17, 'Front Brake: MONROE CX914 Total Solution Ceramic');
+
+--Honda Civic 2018
+INSERT INTO car_brakes (car_type_id, brake_pad)
+    VALUES (18, 'Front Brake: MONROE CX914 Total Solution Ceramic');
+
+--Honda Civic 2019
+INSERT INTO car_brakes (car_type_id, brake_pad)
+    VALUES (19, 'Front Brake: MONROE CX914 Total Solution Ceramic');
+
+--Honda Civic 2020
+INSERT INTO car_brakes (car_type_id, brake_pad)
+    VALUES (20, 'Front Brake: MONROE CX914 Total Solution Ceramic');
+
+
+--Jeep Wrangler 2016
+INSERT INTO car_brakes (car_type_id, brake_pad)
+    VALUES (21, 'Front Brake: MONROE CX1273 Total Solution Ceramic');
+
+--Jeep Wrangler 2017
+INSERT INTO car_brakes (car_type_id, brake_pad)
+    VALUES (22, 'Front Brake: MONROE CX1273 Total Solution Ceramic');
+
+--Jeep Wrangler 2018
+INSERT INTO car_brakes (car_type_id, brake_pad)
+    VALUES (23, 'Front Brake: MONROE CX1273 Total Solution Ceramic');
+
+--Jeep Wragler 2019
+INSERT INTO car_brakes (car_type_id, brake_pad)
+    VALUES (24, 'Front Brake: MONROE CX1273 Total Solution Ceramic');
+
+--Jeep Wrangler 2020
+INSERT INTO car_brakes (car_type_id, brake_pad)
+    VALUES (25, 'Front Brake: MONROE CX1273 Total Solution Ceramic');
