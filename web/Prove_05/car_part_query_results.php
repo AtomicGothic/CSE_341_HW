8d2dd9942a166557dde6f2d473478d9f6ac36a05
AtomicGothic <58697172+AtomicGothic@users.noreply.github.com>
2020-05-22 11:42:07 -0400

    Modified the query.

--- web/Prove_05/car_part_query_results.php
+++ web/Prove_05/car_part_query_results.php
@@ -34,9 +34,8 @@
     }
 
     function searchQuery($name, $db) {
-        // $db=dbConnection();
-        $stmt = $db->prepare("SELECT * FROM car_type, car_brakes WHERE car_type.car_make = :name AND car_type.id = car_brakes.car_type_id");
-        //$name= '$name';
+        //$stmt = $db->prepare("SELECT * FROM car_type, car_brakes WHERE car_type.car_make = :name AND car_type.id = car_brakes.car_type_id");
+        $stmt = $db->prepare("SELECT * FROM car_type, car_brakes WHERE ('query' LIKE '%" . :name . "%') AND car_type.id = car_brakes.car_type_id");
         $stmt->bindValue(':name', $name, PDO::PARAM_STR);
         $stmt->execute();
         $carType = $stmt->fetchAll(PDO::FETCH_ASSOC);
