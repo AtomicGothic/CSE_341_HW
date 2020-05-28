5303958e6104cdc32d0112ca2eb94d82a51b3642
AtomicGothic <58697172+AtomicGothic@users.noreply.github.com>
2020-05-28 13:26:10 -0400

    Added error catching.

--- web/Car_Parts_Assignment/car_part_query_results.php
+++ web/Car_Parts_Assignment/car_part_query_results.php
@@ -4,8 +4,6 @@
     require("dbConnect.php");
     $db = get_db();
 
-
-
     function validateInput($data){
         $data = trim($data);
         $data = stripslashes($data);
@@ -14,13 +12,18 @@
     }
 
     function searchQuery($name, $db) {
-        //$stmt = $db->prepare("SELECT * FROM carType, carBrakes, carType_carBrakes WHERE carType.carMake = :name AND carType.id = carType_carBrakes.carTypeid AND carBrakes.id = carType_carBrakes.carBrakesId");
-        $stmt = $db->prepare("SELECT cb.brakePad FROM carBrakes AS cb JOIN carType_carBrakes AS ctcb ON ctcb.carBrakesID = cb.id JOIN carType AS ct ON ct.id = ctcb.carTypeID WHERE ct.carMake = :name");
-        //$stmt = $db->prepare("SELECT * FROM car_type, car_brakes WHERE ('car_type.car_make' LIKE \"%:name%\") AND car_type.id = car_brakes.car_type_id";
-        $stmt->bindValue(':name', $name, PDO::PARAM_STR);
-        $stmt->execute();
-        $carType = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        return $carType;
+        try{
+            //$stmt = $db->prepare("SELECT * FROM carType, carBrakes, carType_carBrakes WHERE carType.carMake = :name AND carType.id = carType_carBrakes.carTypeid AND carBrakes.id = carType_carBrakes.carBrakesId");
+            $stmt = $db->prepare("SELECT cb.brakePad FROM carBrakes AS cb JOIN carType_carBrakes AS ctcb ON ctcb.carBrakesID = cb.id JOIN carType AS ct ON ct.id = ctcb.carTypeID WHERE ct.carMake = :name");
+            //$stmt = $db->prepare("SELECT * FROM car_type, car_brakes WHERE ('car_type.car_make' LIKE \"%:name%\") AND car_type.id = car_brakes.car_type_id";
+            $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+            $stmt->execute();
+            $carType = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $carType;
+        }
+        catch (Exception $ex) {
+            echo "ERROR: $ex";
+        }
     }
 
     // If the page loads as a POST request, look for this variable, and if it is set
@@ -50,11 +53,15 @@
             echo "<div class='standardText'>";
             //$carParts = $db->query('SELECT car_brakes.brake_pad FROM car_type, car_brakes WHERE car_type.id = car_brakes.car_type_id')
             $i = 0;
-            foreach ($carType as $row)
-            {
-                echo "<input class='main' id=\"item_" . $i . "\" type='checkbox' name='item[]'>" . $row['brakePad'];
-                echo '<br/><br/>';
-                $i++;
+            try {
+                foreach ($carType as $row)
+                {
+                    echo "<input class='main' id=\"item_" . $i . "\" type='checkbox' name='item[]'>" . $row['brakePad'];
+                    echo '<br/><br/>';
+                    $i++;
+                }
+            } catch (Exception $ex) {
+                echo "ERROR: $ex";
             }
             echo "</div>";
         ?>
