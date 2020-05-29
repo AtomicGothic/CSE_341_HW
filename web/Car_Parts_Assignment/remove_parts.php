2f57dd001f039c24467ffb2592d5ee3910133380
AtomicGothic <58697172+AtomicGothic@users.noreply.github.com>
2020-05-28 23:40:15 -0400

    Cleaning up uncessessary code.

--- web/Car_Parts_Assignment/remove_parts.php
+++ web/Car_Parts_Assignment/remove_parts.php
@@ -9,31 +9,22 @@ $db = get_db();
 
 try
 {
-
-    // Delete the referenced ID first.
-    //$carId = $db->lastInsertId("carTypeId_seq");
-
-    $brakeId = $db->prepare("SELECT carBrakesId, id FROM carType_carBrakes, carBrakes WHERE carType_carBrakes.carBrakesId = carBrakes.id AND carBrakes.brakePad = :carBrake");
+    // Prepping the queries. We must delete the row with the foreign key first before we delete the row with the brakes.
     $brakeQuery = "DELETE FROM carBrakes WHERE brakePad = :carBrake";
     $brakeStatement = $db->prepare($brakeQuery);
-
     $IdStatement = $db->prepare("DELETE FROM carType_carBrakes WHERE carBrakesId = (SELECT id FROM carType_carBrakes, carBrakes WHERE carType_carBrakes.carBrakesId = carBrakes.id AND carBrakes.brakePad = :carBrake)");
+
     foreach ($carBrake as $row)
     {
         $cleanedRow = htmlspecialchars($row);
         $brakeStatement->bindValue(':carBrake', $cleanedRow);
-        $brakeId->bindValue(':carBrake', $cleanedRow);
         $IdStatement->bindValue(':carBrake', $cleanedRow);
     }
 
-    //$IdStatement->bindValue(':brakeId', $Id);
-    //$brakeStatement->bindValue(':carTypeId', $carId);
-
+    // Delete the referenced ID first.
+    $IdStatement->execute();
 
     // Delete the brake pad second.
-
-
-    $IdStatement->execute();
     $brakeStatement->execute();
 
 }
